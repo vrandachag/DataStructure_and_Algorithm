738c7e36f8b3804f01b9db36beb8e425351baf22
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2021-02-04 01:41:03 +0530

    Update Avg_salary_of_manager_using_binaryfiles.py

--- Python/Classes-Objects/Avg_salary_of_manager_using_binaryfiles.py
+++ Python/Classes-Objects/Avg_salary_of_manager_using_binaryfiles.py
@@ -1,34 +1,38 @@
 import pickle
 
 class Employee:
-    def __init__(self):
-        f=open("data.txt","ab")
-        self.l = []
-    	name = input("Enter name:")
-	    self.l.append(name)
-	    designation = input("Enter designation:")
-	    self.l.append(designation)
-	    salary = input("Enter salary:")
-	    self.l.append(salary)
-	    pickle.dump(l,f)
-    
-    def avg(self):
-        global sum
-        global count
-        f=open("data.txt","rb")
-        try:
-            while True:
-                l=pickle.load(f)
-                if l[1].upper == 'MANAGER':
-                    sum = sum + int(l[2])
-                    count = count + 1
-        except EOFError:
-        print("Average of salaries of managers is:",sum/count)
-        f.close()
-num = input("Enter number of entries:")
-for i in range(0,int(num)):
+	def __init__(self):
+		self.l = []
+
+	def indata(self):
+		f=open("data.txt","ab")
+		name = input("Enter name:")
+		self.l.append(name)
+		designation = input("Enter designation:")
+		self.l.append(designation)
+		salary = input("Enter salary:")
+		self.l.append(salary)
+		pickle.dump(self.l,f)
 	
-f.close()
+	def avg(self):
+		global sum
+		global count
+		f=open("data.txt","rb")
+		try:
+			while True:
+				self.l=pickle.load(f)
+				if self.l[1].upper() == 'MANAGER':
+					sum = sum + int(self.l[2])
+					count = count + 1
+		except EOFError:
+			print("Average of salaries of managers is:",sum/count)
+			f.close()
 
 sum = 0
 count = 0
+num = input("Enter number of entries:")
+emp = Employee()
+
+for i in range(0,int(num)):
+	emp.indata()
+emp.avg()
