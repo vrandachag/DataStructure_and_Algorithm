335e75985abe7784136626e03f548a04e9a7a70d
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2021-04-07 17:48:07 +0530

    Update LL_implementation_using_xor.cpp

--- Linked List/LL_implementation_using_xor.cpp
+++ Linked List/LL_implementation_using_xor.cpp
@@ -9,56 +9,65 @@ struct node
 
 node *start = NULL;
 
-void Insert()
+node *Insert(node *ret_ptr = NULL)
 {
     if(start == NULL)
     {
         start = new node;
         cout<<"\nEnter value:";
         cin>>start->n;
-        start->next = start;
+        start->next = NULL;
+				return start;
     }
     else
     {
 			cout<<"\nelse";
-			node *p = start,*p2 = NULL;
-			cout<<"\nstart:"<<start;
-			node *k =(struct node*)((uintptr_t)p->next xor (uintptr_t)p);
-			while( k != NULL)
-			{
-				  p = k;
-					k = (struct node*)((uintptr_t)p->next xor (uintptr_t)p);
-			}
+			node *p = ret_ptr,*p2 = NULL;
+//			node *k =(struct node*)((uintptr_t)p->next xor (uintptr_t)p);
 			p2 = new node;
 			cout<<"\nEnter value:";
 			cin>>p2->n;
-			p->next=(struct node*)((uintptr_t)p xor (uintptr_t)p2);
+			node *k=(struct node*)((uintptr_t)(p->next) xor (uintptr_t)p2);
+			p->next = k;
 			p2->next = p;
 			cout<<"\np->next"<<p->next<<"\tp2->next"<<p2->next;
+			return p2;
     }
 }
 
-void Display(node *ptr = start)
+void Display_front(node *ptr = start)
 {
-	node *k =(struct node*)((uintptr_t)ptr->next xor (uintptr_t)ptr);
-	while(k != NULL)
+	node *k = NULL,*ptr1 = ptr;
+	while(ptr1 != NULL)
 	{
-		cout<<"\n"<<ptr->n;
-		ptr = k;
-		k = (struct node*)((uintptr_t)ptr->next xor (uintptr_t)ptr);
-
+		cout<<"\n"<<ptr1->n;
+		ptr = (struct node*)((uintptr_t)ptr->next xor (uintptr_t)k);
+		k = ptr1;
+		ptr1 = ptr;
 	}
-//	cout<<"\n"<<ptr->n;
+
+	cout<<"\nbackward:";
+	ptr1 = k;
+	k = NULL;
+	while(ptr1 != NULL)
+	{
+		cout<<"\n"<<ptr1->n;
+		ptr = (struct node*)((uintptr_t)ptr1->next xor (uintptr_t)k);		
+		k = ptr1;
+		ptr1 = ptr;
+	}
+
 }
 int main()
 {
 	char ch = 'Y';
+	node *p = NULL;
 	while(ch == 'Y' || ch == 'y')
 	{
-		Insert();
+		p = Insert(p);
 		cout<<"\nWant to enter more?";
 		cin>>ch;
 	}
-	Display();
+	Display_front();
   return 0;
 }
