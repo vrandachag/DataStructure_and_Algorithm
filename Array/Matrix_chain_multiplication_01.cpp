c20d3252586bc091a848c12a76571cacbac31fbe
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2021-04-12 18:01:09 +0530

    Create Matrix_chain_multiplication_01.cpp

---
+++ Array/Matrix_chain_multiplication_01.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int matrix_mult(int arr[10][10], vector<int>arr_ele, int x,int y)
+{
+	int s1=INT_MAX,s2 = INT_MAX;
+	if(x==y)
+		return 0;//arr[][] != 0 retrun a[][] 
+	else
+	{
+		if(x != (y-1))
+			s1 = matrix_mult(arr, arr_ele, x, y-1) + (arr_ele[x]*arr_ele[y]*arr_ele[y+1]);
+		if(y+1 < arr_ele.size())
+			s2 = matrix_mult(arr, arr_ele, x+1, y) + (arr_ele[x]*arr_ele[x+1]*arr_ele[y+1]);
+		if(s1<s2)
+		{
+			if(arr[x][y] == 0 || arr[x][y] > s1)
+				arr[x][y] = s1;                 
+		}
+		else
+		{
+			if(arr[x][y] == 0 || arr[x][y] > s2)
+				arr[x][y] = s2;
+		}
+	}
+	return arr[x][y];
+}
+
+
+int main()
+{
+	vector<int>arr_ele;
+	int ele,n,arr[10][10] = {0};
+	cout<<"\nEnter number of elements:";
+	cin>>n;
+	for(int i = 0 ; i < n ; i++)
+	{
+		cin>>ele;
+		arr_ele.push_back(ele);
+	}
+	int answer1 = matrix_mult(arr,arr_ele,0,n-1);
+	for(int i = 0;i<=n;i++)
+	{
+		cout<<"\n";
+		for(int j =0;j<=n;j++)
+		{
+			cout<<arr[i][j]<<"\t";
+		}
+	}
+	cout<<"\nAnswer:"<<answer1<<"\t"<<arr[0][n-2];
+	return 0;
+}
