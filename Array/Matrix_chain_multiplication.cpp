d00d95930788ab38ed210ce4f78ad2ace34dd2d3
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2021-04-13 17:33:03 +0530

    Update Matrix_chain_multiplication.cpp
    https://www.geeksforgeeks.org/matrix-chain-multiplication-dp-8/

--- Array/Matrix_chain_multiplication.cpp
+++ Array/Matrix_chain_multiplication.cpp
@@ -2,80 +2,59 @@
 #include<vector>
 using namespace std;
 
-/*
-int check(vector<int>arr,int x,int y)
+int matrix_mult(int arr[10][10], vector<int>arr_ele, int x,int y)
 {
-	if(arr[x] == arr[y])
-		return 1;
-	else
+	int s1=INT_MAX,s2 = INT_MAX;
+  if(x==y)
 		return 0;
+	else
+	{
+		  if(x != (y-1))
+		   {    
+		    s1 = matrix_mult(arr, arr_ele, x, y-1);
+		    if(y+1 < arr_ele.size())
+    		    s1 = s1 + (arr_ele[x]*arr_ele[y]*arr_ele[y+1]);
+        }
+		   if(y+1 < arr_ele.size())
+		  {
+		    s2 = matrix_mult(arr, arr_ele, x+1, y);
+		    s2+= (arr_ele[x]*arr_ele[x+1]*arr_ele[y+1]);
+        }
+		   if(s1<s2)
+	    	{
+			    if(arr[x][y] == 0 || arr[x][y] > s1)
+				      arr[x][y] = s1;                 
+		    }
+		   else
+	   	 {
+    			if(arr[x][y] == 0 || arr[x][y] > s2)
+		     		arr[x][y] = s2;
+		   }
+	}
+	return arr[x][y];
 }
-*/
 
-int patterns(vector<int>arr,vector<int>&mult,int k,int sum_arr = INT_MAX)
-{
-	int init_x = mult[0],init_y = mult[1],sum = 0,i;
-
-	for(i = mult[1] ; i < arr.size()-1 ; i++)
-	{
-		if(arr[i] == arr[mult[1]])
-		{
-			sum = sum + (arr[mult[0]]*arr[mult[1]]*arr[i+1]);
-			mult[1] = i+1;
-		}
-		else
-			return -1;
-	}
-
-	if(i==arr.size()-1)
-	{
-		if(init_x > 0 || init_y > 1)
-		{
-			for(i = init_y ; i>0 ; i--)
-			{
-				if(arr[i] == arr[mult[1]])
-				{
-					sum = sum + (arr[mult[0]]*arr[mult[1]]*arr[i]);
-					mult[1] = i;
-					}
-			}
-		}
-		else
-			return -1;
-
-		if(sum < sum_arr)
-			sum_arr = sum;
-	}
-
-
-
-
-
-
-
-
-
-	for(int i = 0,j = 1 ; (i < arr.size()) && (j < arr.size() ); i++,j++)
-	{
-		int val = check(arr,i+1,j);
-		if(val == 1)
-		{
-			s1 = s1 + (arr[i]*arr[i+1]*arr[j+1]);
-
-		}
-	}
-}
 
 int main()
 {
-	vector<int>arr,mult;
-	int ele,n;
-	mult.push_back(0);
-	mult.push_back(1);
+	vector<int>arr_ele;
+	int ele,n,arr[10][10] = {0};
+	cout<<"\nEnter number of elements:";
+	cin>>n;
 	for(int i = 0 ; i < n ; i++)
 	{
 		cin>>ele;
-		arr.push_back(ele);
+		arr_ele.push_back(ele);
 	}
+	int answer1 = matrix_mult(arr,arr_ele,0,n-1);
+/*	for(int i = 0;i<=n;i++)
+	{
+		cout<<"\n";
+		for(int j =0;j<=n;j++)
+		{
+			cout<<arr[i][j]<<"\t";
+		}
+	}*/
+	cout<<"\nAnswer:"<<answer1;
 	return 0;
 }
